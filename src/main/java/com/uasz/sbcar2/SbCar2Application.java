2fc350280ca8831b7c850026ca79fb51e0985f71
Mhdiagne <m.d200@zig.univ.sn>
2023-10-27 18:10:03 +0000

    Etat3-lab5-suite

--- src/main/java/com/uasz/sbcar2/SbCar2Application.java
+++ src/main/java/com/uasz/sbcar2/SbCar2Application.java
@@ -35,10 +35,17 @@ public class SbCar2Application implements CommandLineRunner {
 		Owner owner2 = new Owner("Jamal","Musiala");
 		orepository.saveAll(Arrays.asList(owner1,owner2));
 
-		Car car1 = new Car("Ford", "Mustang", "Red", "ADF-1121",2021,59000,owner2);		
-		Car car2 = new Car("Nissan", "Leaf", "white", "SSJ-3002",2019,29000,owner2);		
+		Car car1 = new Car("Ford", "Mustang", "rouge", "ADF-1121",2021,59000,owner2);		
+		Car car2 = new Car("Nissan", "Leaf", "blanc", "SSJ-3002",2019,29000,owner2);		
 		Car car3 = new Car("Toyota", "Prius", "silver", "KKO-0212",2020,39000,owner1);
-		repository.saveAll(Arrays.asList(car1,car2,car3));
+		Car car4 = new Car("Ferrari", "260-kali", "rouge", "AAO-2410",2012,76020,owner1);
+		Car car5 = new Car("Jaguar", "demin", "noir", "zci-1456",2023,69300,owner2);
+		Car car6 = new Car("RolceRoyce", "Culiman", "silver", "KKO-0212",2021,39000,owner1);
+		Car car7 = new Car("Chevrolet", "Camarro", "jaune-noir", "FDG-0657",2014,69950,owner1);
+		Car car8 = new Car("Lambourghini", "358-G", "jaune", "GSH-6734",2018,83040,owner2);
+		Car car9 = new Car("BMW", "X6", "bleu", "HGG-7603",2016,49050,owner1);
+		Car car10 = new Car("RedBullRacing", "RB19", "rouge-jaune-noir", "DFS-6883",2023,39000,owner2);
+		repository.saveAll(Arrays.asList(car1,car2,car3,car4,car5,car6,car7,car8,car9,car10));
 
 		for (Car car : repository.findAll()){
 			logger.info(car.getBrand()+" "+car.getModel());
